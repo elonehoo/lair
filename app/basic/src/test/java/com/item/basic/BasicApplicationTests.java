7bc9d20085a39411ff6efb9d65549e02d541868d
xiaoxunyao <huchengyea@163.com>
2022-05-31 22:55:32 +0800

    chore: init

---
+++ app/basic/src/test/java/com/item/basic/BasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.item.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
