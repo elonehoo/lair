33cb465ed0362f11aabb762d70daa7e3284c2a2a
xiaoxunyao <huchengyea@163.com>
2022-06-01 12:27:15 +0800

    chore: init tasks module

---
+++ app/tasks/src/test/java/com/item/tasks/TasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.item.tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
