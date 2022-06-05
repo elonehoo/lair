5591e39987a375fecc5377773f0cb3703f2606b3
xiaoxunyao <huchengyea@163.com>
2022-06-05 22:55:46 +0800

    chore: not run

--- app/core/src/main/java/com/item/core/CoreApplication.java
+++ app/core/src/main/java/com/item/core/CoreApplication.java
@@ -2,8 +2,12 @@ package com.item.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-@SpringBootApplication
+@EnableCaching
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+@SpringBootApplication(scanBasePackages = {"com.item.*"})
 public class CoreApplication {
 
 	public static void main(String[] args) {
---
+++ app/web/src/main/java/com/item/web/controller/DemoController.java
@@ -0,0 +1,16 @@
+package com.item.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+  @GetMapping("/hello")
+  public String demo1() {
+    return "demo1";
+  }
+
+}
