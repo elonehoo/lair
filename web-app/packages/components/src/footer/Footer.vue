5534ed22ef9ef2d3da713784c73558b7c8d9cb30
xiaoxunyao <huchengyea@163.com>
2022-06-11 00:48:47 +0800

    chore: init `footer` components

---
+++ web-app/packages/components/src/footer/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav text-xl mt-6 inline-flex gap-2>
+    <button class="icon-btn !outline-none">
+      <div i-carbon-moon />
+    </button>
+
+    <a
+      class="icon-btn"
+      i-carbon-logo-github
+      rel="noreferrer"
+      href="https://github.com/antfu/vitesse-lite"
+      target="_blank"
+      title="GitHub"
+    />
+  </nav>
+</template>
