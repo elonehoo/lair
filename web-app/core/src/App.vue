eb7b8d53d5c78cb67cf60a8208c3b1a7b4348626
xiaoxunyao <huchengyea@163.com>
2022-06-10 23:15:40 +0800

    fix: update core

---
+++ web-app/core/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+// import Footer from '@vitem/footer'
+</script>
+
+<template>
+  <main class="font-sans px-4 py-10 text-center gray-700 dark:gray-200">
+    1
+    <!-- <router-view /> -->
+    <!-- <Footer github-link="https://github.com/elonehoo/vitem" /> -->
+  </main>
+</template>
+
+<style>
+</style>
