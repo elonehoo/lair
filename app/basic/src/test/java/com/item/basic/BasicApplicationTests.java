c7c19067414d1030ee4e6c2b028e217091c45c26
xiaoxunyao <huchengyea@163.com>
2022-06-15 23:39:54 +0800

    fix: update core

--- app/basic/src/test/java/com/item/basic/BasicApplicationTests.java
+++ app/basic/src/test/java/com/item/basic/BasicApplicationTests.java
@@ -3,11 +3,11 @@ package com.item.basic;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class BasicApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
 
 }
